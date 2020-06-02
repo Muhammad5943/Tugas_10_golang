ad646af1fbcf1f129eaa8eb139a5eacc9d4f0013
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-06-02 10:20:29 +0700

    Mengumpulkan tugas 10 golang

---
+++ tugas10.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	fmt.Println("Nama 		= Muhammad Aji Putra")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Hobi 		= Ngoding")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Tgl lahir 	= 25 Juni 1995")
+}
